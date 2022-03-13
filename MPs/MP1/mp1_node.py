643d77c9c27ccd79af640a0299466d7473073fc7
MiChan <2417577316@qq.com>
2022-03-13 20:28:02 +0800

    connection complete need interrupt

--- MPs/MP1/mp1_node.py
+++ MPs/MP1/mp1_node.py
@@ -4,31 +4,39 @@ import time
 import threading
 
 
-# the class of broadcast
+# the class of broadcast should send out to everyone else
 class broadcast(threading.Thread):
-    def __init__(self, threadID, name, soc):
+    def __init__(self, threadID, name, soc, nodes_list, n_connected):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.soc = soc
+        self.nodes_list = nodes_list
+        self.n_connected = n_connected
 
     def run(self):
+        # The first part is that it should send to each nodes to make sure the starting-connection
         while True:
             msg = "Hello!"
             msg = msg.encode()
 
-            self.soc.sendto(msg, ("127.0.0.1", 5555))
+            for i in range(len(self.nodes_list)):
+                nodes_i = self.nodes_list[i].split()
+
+                # print(self.name, "sends to", nodes_i[0])
+                self.soc.sendto(msg, (nodes_i[1], int(nodes_i[2])))
 
             time.sleep(5)
 
 
 # the class of receive end
 class receive(threading.Thread):
-    def __init__(self, threadID, name, soc):
+    def __init__(self, threadID, name, soc, n_connected):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.soc = soc
+        self.n_connected = n_connected
 
     def run(self):
         while True:
@@ -39,7 +47,13 @@ class receive(threading.Thread):
                 msg = data.decode()
             except:
                 pass
-            print(self.name, msg)
+            
+            # if the message is the first hello message, send back a ACK and turn the flag
+            if msg == "Hello!":
+                self.n_connected += 1
+                self.soc.sendto("ACK".encode(), addr)
+
+            print(self.name, "received", msg)
 
 
 # the function dealing with input
@@ -76,6 +90,10 @@ def server_set_up(port, n_node, node_id):
 
     print(node_id, "is listening")
 
+    while True:
+        print(node_id, "Still waiting")
+        time.sleep(5)
+
 
 # the function to start connecting other nodes
 def connecting(node_id, n_node, nodes_list):
@@ -93,24 +111,26 @@ def connecting(node_id, n_node, nodes_list):
             s.send(message.encode())
 
 
+# the function when dealing with the input from gentx
+# with a dictionary outside containing the total informations
+def account_modify(message):
+    
+
 # The main function of the node.py
 def main():
     node_id, port, config_file_name = init_node()
 
     n_node, nodes_list = read_config_file(config_file_name)
 
-    # server_set_up(port, n_node, node_id)
-
-    # connecting(node_id, n_node, nodes_list)
+    print(nodes_list)
 
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     soc.bind(("127.0.0.1", port))
-
-    thread_broadcast = broadcast(1, node_id, soc)
-    thread_receive = receive(1, node_id, soc)
-
-    print("init success")
-
+    
+    n_connected = 0
+    thread_broadcast = broadcast(1, node_id, soc, nodes_list, n_connected)
+    thread_receive = receive(2, node_id, soc, n_connected)
+    
     thread_broadcast.start()
     thread_receive.start()
 
